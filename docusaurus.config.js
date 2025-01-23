388a539a2c1ba407897c33f18fd2ab38fa468a55
JulianBlanco007 <julian.blanco@newsol.com.co>
2025-01-23 07:28:22 -0500

    first commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,12 +18,12 @@ const config = {
   url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/prueba-docusaurius/',
+  baseUrl: '/prueba-docusaurus/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'JulianBlanco007', // Usually your GitHub org/user name.
-  projectName: 'prueba-docusaurius', // Usually your repo name.
+  projectName: 'prueba-docusaurus', // Usually your repo name.
  deploymentBranch: "gh-pages",
 
   onBrokenLinks: 'throw',
